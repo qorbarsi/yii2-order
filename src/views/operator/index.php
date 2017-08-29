ab578eef2e4e75f28e68696fe03172b69c932c63
qorbarsi <qorbarsi@gmail.com>
2017-08-29 22:06:15 +0300

    view

--- src/views/operator/index.php
+++ src/views/operator/index.php
@@ -202,21 +202,6 @@ $columns[] = [
                     <?=  \kartik\grid\GridView::widget([
                         'export' => false,
                         'pjax' => true,
-                        //'pjaxSettings'=>[
-                        //    'neverTimeout' => false,
-                        //],
-                        'containerOptions'=>['style'=>'overflow: auto'], // only set when $responsive = false
-                        'headerRowOptions'=>['class'=>'kartik-sheet-style'],
-                        'filterRowOptions'=>['class'=>'kartik-sheet-style'],
-                        'toolbar'=> [
-                            ['content'=>
-                            //Html::button('<i class="glyphicon glyphicon-plus"></i>', ['type'=>'button', 'title'=>Yii::t('kvgrid', 'Add Book'), 'class'=>'btn btn-success', 'onclick'=>'alert("This will launch the book creation form.\n\nDisabled for this demo!");']) . ' '.
-                            Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset', ['grid-demo'], ['data-pjax'=>0, 'class'=>'btn btn-default', 'title'=>Yii::t('order', 'Обновить')])
-                            ],
-                            //'{export}',
-                            '{toggleData}',
-                        ],
-
                         'dataProvider' => $dataProvider,
                         'filterModel' => $searchModel,
                         'columns' => $columns,
